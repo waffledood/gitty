6756ed8109c0baa52d60983ea92040964769115e
waffledood <itshaikalhere@hotmail.sg>
2026-04-23 21:35:23 +0800

    feat: set up persistent input

--- main.ts
+++ main.ts
@@ -1,31 +1,46 @@
 import ok from "./octokit";
 import config from "./config";
 
-import { input } from "@inquirer/prompts";
+import { input, confirm } from "@inquirer/prompts";
 
-if (!config.setupDone) {
-  console.log(
-    "Welcome to gitty! As this is your first time using gitty, please follow through the setup process.",
-  );
+while (true) {
+  if (!config.setupDone) {
+    console.log(
+      "Welcome to gitty! As this is your first time using gitty, please follow through the setup process.",
+    );
 
-  const githubUsername = await input({
-    message: "Enter your GitHub username:",
+    var githubUsername = await input({
+      message: "Enter your GitHub username:",
+    });
+
+    const confirmUsername = await confirm({
+      message: `Your GitHub username is ${githubUsername}`,
+    });
+
+    if (!confirmUsername) {
+      githubUsername = await input({
+        message: "Enter your GitHub username:",
+      });
+    }
+
+    config.username = githubUsername;
+    config.setupDone = true;
+  }
+
+  const response = await ok.request("GET /users/{username}/repos", {
+    username: config.username,
+    headers: {
+      "X-GitHub-Api-Version": "2026-03-10",
+    },
   });
 
-  config.username = githubUsername;
+  const repos: string[] = [];
+
+  response.data.forEach(({ name }) => {
+    repos.push(name);
+  });
+
+  console.log(repos.splice(5).join("\n"));
+
+  break;
 }
-
-const response = await ok.request("GET /users/{username}/repos", {
-  username: config.username,
-  headers: {
-    "X-GitHub-Api-Version": "2026-03-10",
-  },
-});
-
-const repos: string[] = [];
-
-response.data.forEach(({ name }) => {
-  repos.push(name);
-});
-
-console.log(repos.splice(-5).join("\n"));
